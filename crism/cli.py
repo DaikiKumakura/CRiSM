6cba20dcb27f907405a09867dec37a5909165f18
Daiki KUMAKURA <55353329+DaikiKumakura@users.noreply.github.com>
2024-05-25 15:09:40 +0900

    Update cli.py

--- crism/cli.py
+++ crism/cli.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run the CRiSM pipeline for phylogenetic analysis using concatenated ribosomal marker genes.")
     parser.add_argument('-i', '--input', required=True, help="Specify the input directory containing genomic data.")
     parser.add_argument('-o', '--output', required=True, help="Specify the output directory for the analysis results.")
-    parser.add_argument('--hmm', required=True, help="Specify the path to the HMM database file used for gene identification.")
+    parser.add_argument('--db', required=True, help="Specify the path to the HMM database file used for gene identification.")
     parser.add_argument('--list', required=True, help="Specify the path to the list of marker genes.")
     parser.add_argument('-t', '--threads', type=int, default=1, help="Specify the number of CPU threads to use during analysis.")
 
